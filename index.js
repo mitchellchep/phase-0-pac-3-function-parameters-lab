ebc251e0412ccbec0c189b69ebedcc95a797554f
mitchellkoech <mitchikoechi3.7@gmail.com>
2023-09-23 21:21:13 +0300

    Completed assignment
    Signed-off-by: mitchellkoech <mitchikoechi3.7@gmail.com>

---
+++ index.js
@@ -0,0 +1,29 @@
+// Copyright 2023 mitchell
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+function introduction(name){
+    return `Hi, my name is ${name}.`
+}
+
+function introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+function introductionWithLanguageOptional(name, language="JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+function introductionWithLanguageOptional(name, language="JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
